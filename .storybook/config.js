39d50d31071a28d330007eef7059bb8a22372790
Eric M <1023110+erictooth@users.noreply.github.com>
2020-08-15 17:04:03 -0700

    Remove unused dev packages, update deps

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from "@storybook/react";
-
-const req = require.context("../stories", true, /index.tsx$/);
-
-function loadStories() {
-    req.keys().forEach((filename) => req(filename));
-}
-
-configure(loadStories, module);
